2124593e3e21109a093e46cd6813f1e6ac7d3382
ketzeLitvak <ezeilan10@gmail.com>
2023-09-07 04:57:27 +0000

    Separate main in file-system and memory

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <commons/string.h>
+#include <unistd.h>
 #include <config/config.h>
 #include <connection/connection.h>
 #include <handshake/handshake.h>
 #include <initial_configuration/client_start.h>
+#include <initial_configuration/server_start.h>
 
 #define LOGS_CPU "cpu.log"
-#define NUMERO_DE_PORTS 2
 
 int main(int argc, char* argv[]) {
 	t_utils* utils = create_initial_config(argc, argv, LOGS_CPU, true, LOG_LEVEL_TRACE);
@@ -16,38 +16,13 @@ int main(int argc, char* argv[]) {
 	int memory_socket = connect_to_memory(utils);
 	if (memory_socket == -1) return EXIT_FAILURE;
 
-	int server_dispatch_fd;
-	int server_interrupt_fd;
-	char* port_keys[] = { "PUERTO_ESCUCHA_DISPATCH", "PUERTO_ESCUCHA_INTERRUPT" };
-	for (int i = 0; i < NUMERO_DE_PORTS; i++) {
-		char* port_string = port_keys[i];
-		if (!check_if_config_value_exists(utils, port_string)) {
-			utils_destroy(utils);
-			abort();
-		}
-		int fd = start_server(config_get_string_value(utils->config, port_string), utils->logger);
-		switch (i) {
-			case 0:
-				server_dispatch_fd = fd;
-				log_trace(utils->logger, "Se inicio correctamente el server de dispatch");
-			break;
-			case 1:
-				server_interrupt_fd = fd;
-				log_trace(utils->logger, "Se inicio correctamente el server de interrupt");
-			break;
-		}
+	t_conn* ports = start_server_ports(utils);
+	if (ports == NULL) {
+		close(memory_socket);
+		return EXIT_FAILURE;
 	}
 
-	int result_dispatch_handshake = wait_for_initial_handshake_from_kernel(server_dispatch_fd, utils->logger);
-	if (result_dispatch_handshake == -1) {
-		utils_destroy_with_connection(utils, memory_socket);
-	    return EXIT_FAILURE;
-	}
-	int result_interrupt_handshake = wait_for_initial_handshake_from_kernel(server_interrupt_fd, utils->logger);
-	if (result_interrupt_handshake == -1) {
-		utils_destroy_with_connection(utils, memory_socket);
-	    return EXIT_FAILURE;
-	}
-
+	utils_destroy_with_connection(utils, memory_socket);
+	free(ports);
     return 0;
 }
