109f302ff619ce8b2e176309723fee00899f8679
Brunosaurio <bdeangelis@frba.utn.edu.ar>
2023-09-21 15:31:49 +0000

    solucionado page fault

--- kernel/src/planificadores/planificador.c
+++ kernel/src/planificadores/planificador.c
@@ -84,7 +84,6 @@ void listar_proceso(t_list* lista){
 }
 
 void listar_procesos_por_estado(){
-	int i;
 	printf("Listado procesos en NEW:\n");
 	listar_proceso(lista_estado_NEW);
 	printf("Listado procesos en READY:\n");
@@ -92,10 +91,9 @@ void listar_procesos_por_estado(){
 	printf("Listado procesos en EXIT:\n");
 	listar_proceso(lista_estado_EXIT);
 	printf("Proceso en EXECUTE: %d", estado_EXEC->pid);
-	uint32_t dispSalidaAux = dispDeSalida;
-	for(int i = 0; i <= dispDeSalida - 1; i++){
-		printf("Listado procesos en BLOCKED disp %d", i);
-		listar_proceso(cola_estado_BLOCKED[i]);
+	for(int j = 0; j <= dispDeSalida - 1; j++){
+		printf("Listado procesos en BLOCKED disp %d", j);
+		listar_proceso(cola_estado_BLOCKED[j]);
 	}
 
 }
